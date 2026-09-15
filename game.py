795c1402b565a4f96e78d9a294b30bd94aec704b
Elibadgley <eli.badgley@gmail.com>
2026-09-15 08:54:50 -0500

    BUILT HERO

--- game.py
+++ game.py
@@ -7,27 +7,33 @@ ARENA_NAME = "The Colosseum"
 
 
 def main():
+#creating the arena
     """Open the arena and introduce its first opponent."""
     print(f"Welcome to {ARENA_NAME}!")
     print("༼ ᓄºل͟º ༽ᓄ   ᕦ(ò_óˇ)ᕤ")
     print("The gates are opening...")
 
+#Creating first hero
     hero = Hero("Mel Gibson")
     print(f"Hero {hero.name} has answered the call and will save this world!")
     print(f"{hero.name} enters the arena with {hero.health} health.")
 
+#Creating the First Goblin
     goblin = Goblin("Edward Longshanks I")
 
     print(f"{goblin.name} enters the arena with {goblin.health} health.")
 
+#creating the second Goblin
     goblinTwo = Goblin("Edward Longshanks II")
     
     print(f"{goblinTwo.name} enters the arena with {goblinTwo.health} health.")
+
  #The Hero attacks a goblin
     heroAttack = hero.attack(random.randint(1,12))
     goblin.take_damage(heroAttack)
     if heroAttack > 12:
         print("CRIT")
+
 #The gobiln  attacks the hero
     goblinAttack = goblin.attack()
     hero.take_damage(goblinAttack)

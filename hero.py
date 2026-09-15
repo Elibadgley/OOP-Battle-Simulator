e7fbc340fe5f2a3a754a906356922ddf54698b95
Elibadgley <eli.badgley@gmail.com>
2026-09-15 09:59:07 -0500

    Fixed damage

--- hero.py
+++ hero.py
@@ -7,11 +7,19 @@ class Hero:
         self.attack_power = random.randint(1, 12)
         self.crit = random.randint(1, 12)
 
+<<<<<<< Updated upstream
     def attack(self, crit):
         if self.attack_power >= 11:
             return self.attack_power + self.crit
+=======
+    def attack(self):
+        doesCrit =  random.randint(1, 20) == 20
+        attack = random.randint(1, self.attack_power)
+        if doesCrit:
+            return attack + self.crit
+>>>>>>> Stashed changes
         else:
-            return self.attack_power
+            return attack
 
     def take_damage(self, damage):
         self.health = max(0, self.health - damage)

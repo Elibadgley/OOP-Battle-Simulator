363d4d939bdb676eca50562253bec95e499521af
Elibadgley <eli.badgley@gmail.com>
2026-09-15 10:53:29 -0500

    Creating additional classes
    creating the rogue, warrior, sorcerer, and ranger classes for more variation.

--- heroClasses.py
+++ heroClasses.py
@@ -5,6 +5,7 @@ class Warrior:
     def __init__(self, name):
         self.name = name
         self.health = 200
+<<<<<<< Updated upstream
         self.attack_power = random.randint(1, 12)
         self.crit = random.randint(1, 12)
 
@@ -20,6 +21,25 @@ class Warrior:
         
 
     def isAlive(self):
+=======
+        self.attack_power = 12
+        self.crit = random.randint(1, 12)
+
+    def attack(self):
+        for i in range(2):
+            doesCrit =  random.randint(1, 20)
+            if doesCrit == 20 or doesCrit == 19:
+                print("CRITICAL HIT")
+                return random.randint(1, self.attack_power) + self.crit + i
+            else:
+                return random.randint(1, self.attack_power) + i
+
+    def take_damage(self, damage):
+        self.health = max(0, (self.health - damage) / 2)
+        print(f"{self.name} takes {damage / 2} damage. Health: {self.health}")
+
+    def is_alive(self):
+>>>>>>> Stashed changes
         if self.health > 0:
             return True
         else:
@@ -27,7 +47,34 @@ class Warrior:
 
 
 class Sorcerer:
+<<<<<<< Updated upstream
     pass
+=======
+    def __init__(self, name):
+        self.name = name
+        self.health = 120
+        self.attack_power = 12
+        self.crit = random.randint(1, 12)
+    
+    def attack(self):
+        doesCrit =  random.randint(1, 20)
+        if doesCrit == 20:
+            print("CRITICAL HIT")
+            return random.randint(1, self.attack_power) + self.crit
+        else:
+            return random.randint(1, self.attack_power)
+    
+    def take_damage(self, damage):
+        self.health = max(0, self.health - damage)
+        print(f"{self.name} takes {damage} damage. Health: {self.health}")
+            
+    
+    def is_alive(self):
+        if self.health > 0:
+            return True
+        else:
+            return False
+>>>>>>> Stashed changes
 
 class Rogue:
     pass

5aaf0f5c34d70447b310d9e1467616aa0a964615
Elibadgley <eli.badgley@gmail.com>
2026-09-16 11:53:02 -0500

    Game.py Version 1.0

--- game.py
+++ game.py
@@ -5,6 +5,13 @@ from heroClasses import Warrior, Rogue, Ranger
 
 ARENA_NAME = "The Colosseum"
 
+def hero_interface(hero: Hero):
+    print(f"{hero.name} Interface:")
+    print(" ")
+    print(f"Attack Power: {hero.attack_power} ")
+    print(f"Max Health: {hero.health}")
+    print("Crit Rate: 5%")
+
 def battle(hero: Hero, enemy: Goblin):
     while hero.is_alive() and enemy.is_alive():
         hero_damage = hero.attack()
@@ -21,31 +28,27 @@ def battle(hero: Hero, enemy: Goblin):
 
         
 def main():
-#creating the arena
-    """Open the arena and introduce its first opponent."""
-    print(f"Welcome to {ARENA_NAME}!")
-    print("༼ ᓄºل͟º ༽ᓄ   ᕦ(ò_óˇ)ᕤ")
-    print("The gates are opening...")
-
-#Creating first hero
+#Creating monsters
     hero = Warrior("Mel Gibson")
-    print(f"Hero {hero.name} has answered the call and will save this world!")
-    print(f"{hero.name} enters the arena with {hero.health} health.")
-
-#Creating the First Goblin
     goblin = Goblin("Goblin I")
-
-    print(f"{goblin.name} enters the arena with {goblin.health} health.")
-
-#creating the second Goblin
     goblinTwo = Goblin("Goblin II")
+
+#Ask about UI
+    userInput = input("Would you like to battle? (y/n)")
+    if userInput.lower() == "y":
+        #Create the Arena
+        """Open the arena and introduce its first opponent."""
+        print(f"Welcome to {ARENA_NAME}!")
+        print("༼ ᓄºل͟º ༽ᓄ   ᕦ(ò_óˇ)ᕤ")
+        print("The gates are opening...")
     
-    print(f"{goblinTwo.name} enters the arena with {goblinTwo.health} health.")
+        print(f"{hero.name} enters the arena with {hero.health} health.")  
+        print(f"{goblin.name} enters the arena with {goblin.health} health.")
+        print(f"{goblinTwo.name} enters the arena with {goblinTwo.health} health.")
+        battle(hero, goblin)
 
-#permanent funtion for battle
-    battle(hero, goblin)
-
-
+    elif userInput.lower() == "n":
+        hero_interface(hero)
 
 
 if __name__ == "__main__":
